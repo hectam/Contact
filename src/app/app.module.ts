db7b3a3653d665d363e8a3680f8cabfa88c28b47
hectam <hectortabora@unitec.edu>
2021-05-14 03:06:04 -0500

    Primera Version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RootComponent } from './root/root.component';
+import { ListContactosComponent } from './list-contactos/list-contactos.component';
+import { EditContactosComponent } from './edit-contactos/edit-contactos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RootComponent,
+    ListContactosComponent,
+    EditContactosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [RootComponent]
 })
 export class AppModule { }
